879b1769924c88da12bd9b19e473f88f98b06267
lucasOliveiraDeveloper <lucas.gabrielsilva2011@gmail.com>
2025-12-08 18:38:07 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/lucasdev/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.lucasdev.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucasdev.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+	
+}
---
+++ src/main/java/com/lucasdev/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.lucasdev.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucasdev.workshopmongo.domain.User;
+import com.lucasdev.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
