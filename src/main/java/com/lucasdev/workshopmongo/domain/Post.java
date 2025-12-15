3023f35c65ec6436a4488b467bbaa112db4d1520
lucasOliveiraDeveloper <lucas.gabrielsilva2011@gmail.com>
2025-12-15 16:06:48 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/lucasdev/workshopmongo/domain/Post.java
+++ src/main/java/com/lucasdev/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.lucasdev.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.lucasdev.workshopmongo.dto.AuthorDTO;
+import com.lucasdev.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -20,6 +23,9 @@ public class Post implements Serializable{
 	private String body;
 	private AuthorDTO author;
 	
+	private List<CommentDTO> comments = new ArrayList<>();
+
+	
 	public Post() {
 		
 	}
@@ -31,6 +37,7 @@ public class Post implements Serializable{
 		this.title = title;
 		this.body = body;
 		this.author = author;
+		
 	}
 
 	public String getId() {
@@ -73,6 +80,13 @@ public class Post implements Serializable{
 		this.author = author;
 	}
 	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -90,6 +104,7 @@ public class Post implements Serializable{
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
+
 
 	
 	
